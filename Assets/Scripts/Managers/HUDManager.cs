19f24a0cee98f91fae32ea33384070e65e132483
21diego <morinigodiego21@gmail.com>
2022-09-06 23:07:41 -0300

    puliendo la barra de vida

--- Assets/Scripts/Managers/HUDManager.cs
+++ Assets/Scripts/Managers/HUDManager.cs
@@ -7,6 +7,8 @@ public class HUDManager : MonoBehaviour
     private static HUDManager instance;
     public static HUDManager Instance { get => instance; }
     [SerializeField] private Slider hpBar;
+    private Dictionary<string, Color32> statusHPColor;
+    private Image fillHPImage;
 
     private void Awake()
     {
@@ -18,16 +20,31 @@ public class HUDManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        statusHPColor = new Dictionary<string, Color32>(){
+            {"safe", new Color32(89, 202, 54, 225)},
+            {"warning", new Color32(177, 200, 0, 225)},
+            {"danger", new Color32(224, 27, 0, 225)},
+        };
+
+        fillHPImage = instance.hpBar.transform.GetChild(1).transform.GetChild(0).GetComponent<Image>();
     }
 
     public static void SetHPBar(int newValue)
     {
         instance.hpBar.value = newValue;
-        if (instance.hpBar.value < 60){
-            instance.hpBar.transform.GetChild(1).transform.GetChild(0).GetComponent<Image>().color = new Color32(177, 200, 0, 225);
+        
+        if (instance.hpBar.value >= 75)
+        {
+            instance.fillHPImage.color = instance.statusHPColor["safe"];
         }
-        if (instance.hpBar.value <= 20){
-            instance.hpBar.transform.GetChild(1).transform.GetChild(0).GetComponent<Image>().color = new Color32(224, 27, 0, 225);
+        if (instance.hpBar.value < 75 && instance.hpBar.value > 30)
+        {
+            instance.fillHPImage.color = instance.statusHPColor["warning"];
+        }
+        if (instance.hpBar.value <= 30)
+        {
+            instance.fillHPImage.color = instance.statusHPColor["danger"];
         }
     }
 }
