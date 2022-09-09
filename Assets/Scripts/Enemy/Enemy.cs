51b0d1d5469ad9e149a7a4f18ea68c28f6a0379e
21diego <morinigodiego21@gmail.com>
2022-09-09 00:28:18 -0300

    Puliendo herencia

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] int damage;
+    [SerializeField] int health;
+    [SerializeField][Range(1f, 5f)] float speedMovement = 2f;
+    [SerializeField] Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected virtual void Movement(){
+        LookAtPlayer();
+    }
+
+    protected virtual void Attack(){
+
+    }
+
+    protected void LookAtPlayer()
+    {
+        // Determino la nueva rotacion
+        Quaternion newRotation = Quaternion.LookRotation(playerTransform.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, 1.5f * Time.deltaTime);
+    }
+}
