1ddc8c874865253e5e4047e111b4f7117c32bbc1
21diego <morinigodiego21@gmail.com>
2022-09-21 00:13:12 -0300

    efecto global permanente

--- Assets/Scripts/Managers/HealthFilter.cs
+++ Assets/Scripts/Managers/HealthFilter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
-public class GlobalVolumeManger : MonoBehaviour
+public class HealthFilter : MonoBehaviour
 {
     private PostProcessVolume globalVolume;
     Vignette healthFilter;
